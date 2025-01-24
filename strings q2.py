16a515e4c5d1680197f11b5c247d235845470519
hex021 <168072860+hex021@users.noreply.github.com>
2025-01-24 12:06:08 +0530

    Add files via upload

---
+++ strings q2.py
@@ -0,0 +1,11 @@
+string = input("Enter a string: ")
+print("original string: ", string)
+
+lower_case_string = string.lower()
+print("Lowercase:", lower_case_string)
+
+upper_case_string = string.upper()
+print("Uppercase:", upper_case_string)
+
+toggle_case_string = string.swapcase()
+print("Toggle Case:", toggle_case_string)
