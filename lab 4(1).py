1c4f2e360aed45eff1941772e0dab1faa6c61ad2
hex021 <168072860+hex021@users.noreply.github.com>
2025-01-31 11:54:02 +0530

    Add files via upload

---
+++ lab 4(1).py
@@ -0,0 +1,8 @@
+print("Uppercase Alphabets:")
+for i in range(65, 91):
+    print(chr(i),end=' ')
+print()
+print("Lowercase Alphabets:")
+for i in range(97, 123):
+    print(chr(i),end=' ')
+print()
